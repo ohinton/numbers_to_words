634749f01b828ffab2cb4bec9346b63f568962fc
Olivia Hinton and Stephanie Gurung <olivia@>
2016-06-28 10:34:08 -0700

    spec translates numbers between 20-99 to a word

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -2,15 +2,31 @@ require('pry')
 
 class String
   define_method(:numbers_to_words) do
-    output = ""
+    final_output = ""
     number = self.to_i()
-    single_digit_numbers = {1=>"one", 2=>"two", 3=>"three", 4=>"four", 5=>"five", 6=>"six", 7=>"seven", 8=>"eight", 9=>"nine"}
-    single_digit_integers = single_digit_numbers.keys()
-    single_digit_integers.each() do |int|
-      if number.==(int)
-        output = single_digit_numbers.fetch(int)
+    ones_digit = number.%(10)
+    tens_digit = number./(10)
+
+    ones_place_numbers = {1=>"one", 2=>"two", 3=>"three", 4=>"four", 5=>"five", 6=>"six", 7=>"seven", 8=>"eight", 9=>"nine"}
+    ones_place_integers = ones_place_numbers.keys()
+
+    tens_place_numbers = {2=>"twenty", 3=>"thirty", 4=>"forty", 5=>
+    "fifty", 6=>"sixty", 7=>"seventy", 8=>"eighty", 9=>"ninety"}
+    tens_place_integers = tens_place_numbers.keys()
+
+    tens_place_integers.each() do |int|
+      if tens_digit.==(int)
+        tens_output = tens_place_numbers.fetch(int)
+        tens_output.concat(" ")
+        final_output = tens_output
       end
     end
-    output
+    ones_place_integers.each() do |int|
+      if ones_digit.==(int)
+        ones_output = ones_place_numbers.fetch(int)
+        final_output.concat(ones_output)
+      end
+    end
+    final_output
   end
 end
--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -4,4 +4,7 @@ describe('numbers_to_words') do
   it("translates a single digit number to a word") do
     expect("1".numbers_to_words()).to(eq("one"))
   end
+  it("translates a numbers between 20-99 to a word") do
+    expect("21".numbers_to_words()).to(eq("twenty one"))
+  end
 end
